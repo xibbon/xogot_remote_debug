27dcf52181228b2558c0db9a7939ed5bcd91b52d
James Clancey <james.clancey@gmail.com>
2025-10-30 13:00:56 -0800

    Add iOS virtual controller settings configuration
    Introduces a new method to set default iOS virtual controller settings in ProjectSettings if they do not already exist. This ensures required input options are available for iOS builds and saves the settings only when new ones are added. Fixes #5

--- addons/xogot_remote_debugger/xogot_remote_debugger.gd
+++ addons/xogot_remote_debugger/xogot_remote_debugger.gd
@@ -9,6 +9,9 @@ func _enter_tree():
 	if OS.has_feature("mobile"):
 		return
 
+	# Configure iOS virtual controller settings
+	_configure_ios_virtual_controller()
+
 	xogot_export_platform = preload("res://addons/xogot_remote_debugger/xogot_export_platform.gd").new()
 	add_export_platform(xogot_export_platform)
 	Engine.register_singleton("XogotExportPlatform", xogot_export_platform)
@@ -34,6 +37,43 @@ func _exit_tree():
 	remove_control_from_docks(dock)
 	# Erase the control from the memory.
 	dock.free()
+
+func _configure_ios_virtual_controller():
+	# Set iOS virtual controller default settings only if they don't exist
+	var settings_added = false
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_controller"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_controller", false)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_left_thumbstick"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_left_thumbstick", true)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_right_thumbstick"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_right_thumbstick", true)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_button_a"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_button_a", true)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_button_b"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_button_b", true)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_button_x"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_button_x", true)
+		settings_added = true
+
+	if not ProjectSettings.has_setting("input_devices/virtual_controller/ios/enable_button_y"):
+		ProjectSettings.set_setting("input_devices/virtual_controller/ios/enable_button_y", true)
+		settings_added = true
+
+	# Only save if we actually added new settings
+	if settings_added:
+		ProjectSettings.save()
+		print("Xogot: Configured iOS virtual controller settings")
 
 func _notification(what):
 	# print("Notification: ", what)
